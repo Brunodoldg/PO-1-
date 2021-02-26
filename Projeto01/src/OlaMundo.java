34f2c67f03ae835a8fd5c41a1443d3d1dbe8f873
Bruno <trabalhos_brunogit config --global user.name Your>
2021-02-26 19:56:45 -0300

    Alterações

--- Projeto01/src/OlaMundo.java
+++ Projeto01/src/OlaMundo.java
@@ -1,8 +1,12 @@
+import javax.swing.*;
+
 public class OlaMundo {
 
     public static void main(String[] args) {
         System.out.println("Ola Mundo!");
         System.out.println("Eu Sou o Bruno!!");
+        JOptionPane.showInputDialog(null, "Qual seu time?");
+
     }
 
 }
