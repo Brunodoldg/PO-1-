82e8106a665af782dd637ca6b51fead3c1b4adae
Bruno <trabalhos_brunogit config --global user.name Your>
2021-02-26 19:04:52 -0300

    Teste

--- Projeto01/src/OlaMundo.java
+++ Projeto01/src/OlaMundo.java
@@ -2,7 +2,7 @@ public class OlaMundo {
 
     public static void main(String[] args) {
         System.out.println("Ola Mundo!");
-        System.out.println("Eu Sou o Bruno!");
+        System.out.println("Eu Sou o Bruno!!");
     }
 
 }
