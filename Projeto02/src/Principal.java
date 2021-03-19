682a69a2ca596dedad790e95da88933fd5d84d63
Bruno <trabalhos_brunogit config --global user.name Your>
2021-03-19 17:27:38 -0300

    Alterações projeto 02

--- Projeto02/src/Principal.java
+++ Projeto02/src/Principal.java
@@ -2,6 +2,7 @@ public class Principal {
 
     public static void main (String [] args) {
     Aluno aluno1 = new Aluno();
+    Professor professor1 = new Professor();
 
     aluno1.setNome("Bruno");
     aluno1.setMatricula("04066065");
@@ -12,6 +13,10 @@ public class Principal {
     System.out.println("Matricula: " + aluno1.getMatricula());
     System.out.println("Altura: " + aluno1.getAltura());
     System.out.println("Idade: " + aluno1.getIdade());
+
+    professor1.setSalario(1000.00);
+
+    System.out.println("O salario é: " + professor1.getSalario());
 
     }
 }
